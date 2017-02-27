b280527b5b7dbde6a2c2e6008dec395a9eef3359
Alexander Krutov <krutov@tecomgroup.ru>
2017-02-27 15:49:10 +0300

    Fixed #2 - "paging" option causes exception

--- DataTables.Queryable/Properties/AssemblyInfo.cs
+++ DataTables.Queryable/Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
 [assembly: AssemblyInformationalVersion("1.1.0")]
