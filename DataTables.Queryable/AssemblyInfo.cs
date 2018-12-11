817be59fbe7d66313709b5d374fd49c84705478f
Alexander Krutov <krutov@tecomgroup.ru>
2018-12-11 10:46:52 +0300

    Version updated

--- DataTables.Queryable/AssemblyInfo.cs
+++ DataTables.Queryable/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 
-[assembly: AssemblyVersion("1.7.2")]
-[assembly: AssemblyFileVersion("1.7.2")]
-[assembly: AssemblyInformationalVersion("1.7.2")]
+[assembly: AssemblyVersion("1.7.3")]
+[assembly: AssemblyFileVersion("1.7.3")]
+[assembly: AssemblyInformationalVersion("1.7.3")]
